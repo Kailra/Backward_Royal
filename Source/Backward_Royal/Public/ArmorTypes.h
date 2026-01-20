a51f8a91b08f1c3c609ba9ed7f68ab411734aec2
Postick <csj729@gmail.com>
2026-01-20 13:52:18 +0900

    ArmorType 변경 중, 임시 업로드

--- Source/Backward_Royal/Public/ArmorTypes.h
+++ Source/Backward_Royal/Public/ArmorTypes.h
@@ -5,15 +5,6 @@
 #include "ArmorTypes.generated.h"
 
 UENUM(BlueprintType)
-enum class EArmorType : uint8
-{
-    Plate,
-    Leather,
-    Cloth,
-    None
-};
-
-UENUM(BlueprintType)
 enum class EArmorSlot : uint8
 {
     Head,   // 머리
@@ -36,17 +27,5 @@ struct FArmorData : public FTableRowBase
     USkeletalMesh* ArmorMesh; // 적용할 메쉬
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    EArmorType Type;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
     EArmorSlot EquipSlot;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float DefensePower; // 방어력
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float WeightKg; // 무게 (속도에 영향)
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float Durability; // 내구도
 };
