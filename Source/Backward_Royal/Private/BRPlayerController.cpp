540d238e20ccf5ec59e306865a2171c7acc603ff
Postick <csj729@gmail.com>
2026-02-11 13:58:46 +0900

    HP바 UI에 연결, SwitchOrb 트리거 시 지터링, 상체 플레이어 조작 안되는 경우, 커마 적용 수정 진행 중

--- Source/Backward_Royal/Private/BRPlayerController.cpp
+++ Source/Backward_Royal/Private/BRPlayerController.cpp
@@ -1518,14 +1518,11 @@ void ABRPlayerController::SetupRoleInput(bool bIsLower)
 		}
 	}
 
-	// [추가] 하체 캐릭터라면 입력 바인딩(함수 연결)을 강제로 다시 시키기
-	if (bIsLower)
+	// 하체 캐릭터라면 입력 바인딩(함수 연결)을 강제로 다시 시키기
+	if (APawn* P = GetPawn())
 	{
-		if (APawn* P = GetPawn())
-		{
-			// 클라이언트에게 입력 시스템 재시작 명령
-			ClientRestart(P);
-		}
+		// 클라이언트에게 입력 시스템 재시작 명령 (SetupPlayerInputComponent 재호출 유도)
+		ClientRestart(P);
 	}
 }
 
